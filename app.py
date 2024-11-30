729219b1700081038df87df96be23bf7cdfc26f4
Alejoho <alejoreilly96@gmail.com>
2024-11-30 08:19:04 -0500

    Solve the problem of the double get

--- app.py
+++ app.py
@@ -1,26 +1,13 @@
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, Length
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 
 
-class LoginForm(FlaskForm):
-    name = StringField(
-        "Name", validators=[Length(5, 10, "Need to be between 5 and 10")]
-    )
-    age = IntegerField("Age")
-    sex = StringField("Sex")
-    submit = SubmitField("Submit")
-
-
 @app.route("/", methods=["GET", "POST"])
 def index():
-    form = LoginForm()
-
-    return render_template("index.html", form=form)
+    return render_template("test.html")
 
 
 if __name__ == "__main__":
